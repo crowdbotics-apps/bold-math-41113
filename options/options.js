1219209b15fa319ef2b7ca68fc41721c04a88e29
Crowdbotics <team@crowdbotics.com>
2023-05-31 01:16:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-math-41113",
+  url: "https://bold-math-41113.botics.co",
+  api: "https://bold-math-41113.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
